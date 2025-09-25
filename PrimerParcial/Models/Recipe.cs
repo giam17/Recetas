bcd799d8abebe22d9c5f14d38557f1fb4cd87f42
Giam <tejerinalucass4@gmail.com>
2025-09-25 19:34:48 -0400

    CreandoModeloRecipe

--- PrimerParcial/Models/Recipe.cs
+++ PrimerParcial/Models/Recipe.cs
@@ -1,6 +1,29 @@
-﻿namespace PrimerParcial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
 {
     public class Recipe
     {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Title { get; set; } = "";
+
+        [Required]
+        public string Description { get; set; } = "";
+
+        public int PreparationTimeMinutes { get; set; }
+
+        public int Servings { get; set; }
+
+        [Required]
+        public string Instructions { get; set; } = "";
+
+        public DateTime DateCreated { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } = null!;
+        public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
     }
 }
